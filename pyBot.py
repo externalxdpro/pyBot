563068a15d572e1a7adbcd0b79e9ab4b35c0f358
externalxdpro <externalxdpro@gmail.com>
2022-06-24 19:06:43 -0400

    Add slash commands

--- pyBot.py
+++ pyBot.py
@@ -3,11 +3,13 @@
 import os
 import logging
 import disnake
+from disnake.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 token = os.getenv("TOKEN")
+guildId = os.getenv("GUILDID")
 
 logger = logging.getLogger('disnake')
 handler = logging.FileHandler(filename='pyBot.log', encoding='utf-8', mode='w')
@@ -15,20 +17,14 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 intents = disnake.Intents.all()
-client = disnake.Client(intents=intents)
+client = commands.InteractionBot(test_guilds=[int(guildId)], intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    print(f'Message from {message.author}: {message.content}')
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.slash_command()
+async def hello(inter):
+    await inter.response.send_message('Hello!')
 
 client.run(token)
