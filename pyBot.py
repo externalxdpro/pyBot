3f53c64f2328c4828bf77760e73a041ce135f990
externalxdpro <externalxdpro@gmail.com>
2022-06-25 09:25:26 -0400

    Add comments

--- pyBot.py
+++ pyBot.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
+# Imports
 import os
 import logging
 import disnake
 from disnake.ext import commands
 from dotenv import load_dotenv
 
+# Get environment variables from .env
 load_dotenv()
-
 token = os.getenv("TOKEN")
 guildId = os.getenv("GUILDID")
 
+# Set up logger
 logger = logging.getLogger('disnake')
 handler = logging.FileHandler(filename='pyBot.log', encoding='utf-8', mode='w')
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
+# Set up intents and client
 intents = disnake.Intents.all()
 client = commands.InteractionBot(test_guilds=[int(guildId)], intents=intents)
 
